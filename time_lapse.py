ae5dc09522c81e151af63d167ac1953682df3814
joegenius98 <jo112095@gmail.com>
2019-07-23 16:23:51 -0400

    Completed time lapse

--- time_lapse.py
+++ time_lapse.py
@@ -10,14 +10,25 @@ import time
 # Enter the path where the raspberry pi will store its file
 RASPI_PATH = ""
 
-INTERVAL = None# the time interval (in seconds) between pictures
-SESSION_LENGTH = None# the duration of the script
+INTERVAL = 5# the time interval (in seconds) between pictures
+SESSION_LENGTH = 600# the duration of the script
+PHOTOS = SESSION_LENGTH / INTERVAL
 
-camera = PiCamera()
-camera.start_preview()
+# camera = PiCamera()
+# camera.start_preview()
 
 
 # Have the camera take pictures at the specified interval until the session is over
+with PiCamera() as camera:
+    camera.start_preview()
+    try:
+        for i, filename in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
+            print(filename)
+            time.sleep(INTERVAL)
+            if i == PHOTOS - 1:
+                break
+    finally:
+        camera.stop_preview()
 
 
 camera.stop_preview()
